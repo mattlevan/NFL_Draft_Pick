ffae83ef93781974c8ce9c372646c9b9ddd63bae
Matt Levan <odds@matt-laptop.local>
2015-09-09 18:09:59 -0700

    Initial commit

---
+++ Draft.java
@@ -0,0 +1,21 @@
+/*
+ * NFL Draft Pick
+ * 
+ * Matt Levan
+ *
+ * Project 1
+ * CSC 311 Data Structures
+ * Dr. Amlan Chatterjee
+ *
+ * Draft.java
+ * Driver class for NFL Draft Pick program.
+ *
+ */
+
+import java.util.*;
+
+public class Draft {
+    public static void main(String[] args) {
+        
+    }
+}
